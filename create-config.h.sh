b087dabf3d107ca9ff2e78c94bbab344af589ab2
Jussi Judin <jjudin+largest-tmpfs@iki.fi>
2018-06-22 11:45:24 +0300

    Add include guards to config.h

--- create-config.h.sh
+++ create-config.h.sh
@@ -2,5 +2,14 @@
 
 set -eu
 
-echo "#define LARGEST_TMPFS_VERSION \"$(cat "$1")\"" > "$2"
-echo "#define LARGEST_TMPFS_SYSTEM \"$(uname)\"" >> "$2"
+VERSION_FILE=$1
+OUT_FILE=$2
+{
+    echo "#ifndef LARGEST_TMPFS_CONFIG_H"
+    echo "#define LARGEST_TMPFS_CONFIG_H"
+    echo
+    echo "#define LARGEST_TMPFS_VERSION \"$(cat "$VERSION_FILE")\"" || exit 1
+    echo "#define LARGEST_TMPFS_SYSTEM \"$(uname)\""
+    echo
+    echo "#endif"
+} > "$OUT_FILE"
